5ee50b9d174b426f186834257d5df1266933919d
7x <info@se7enx.com>
2024-01-28 22:32:46 -0800

    Create ezinfo.php

---
+++ ezinfo.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * File containing the sevenx-themes-simple copyright information file
+ *
+ * @copyright Copyright (C) 1999 - 2024 7x. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2 (or later)
+ * @version //autogentag//
+ * @package sevenx-themes-simple
+ */
+
+class sevenx-themes-simpleInfo
+{
+    public static function info()
+    {
+        return array( 'Name' => "<a href='https://github.com/se7enxweb/sevenx-themes-simple">7x Themes : Simple</a>",
+                      'Version' => "1.0.1",
+                      'Copyright' => "Copyright (C) 1999 - 2024 <a href='https://se7enx.com' title='7x'>Brookins Consulting</a>",
+                      'License' => "GNU General Public License v2.0 (or later)",
+                      'info_url' => "https://github.com/se7enxweb/sevenx-themes-simple"
+                    );
+    }
+}
+
+?>
