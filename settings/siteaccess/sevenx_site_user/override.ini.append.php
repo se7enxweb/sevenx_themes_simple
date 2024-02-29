c6a86155d8d82167d79334eb0f12fa23d9bc9af7
7x <info@se7enx.com>
2024-02-29 14:48:24 +0000

    Added: Template override extension settings for default siteaccess

---
+++ settings/siteaccess/sevenx_site_user/override.ini.append.php
@@ -0,0 +1,18 @@
+<?php /* #?ini charset="utf-8"?
+
+[embed_store]
+Source=node/view/embed.tpl
+MatchFile=full/folder.tpl
+Subdir=templates
+Match[class_identifier]=folder
+Match[node]=226
+
+[full_video]
+Source=node/view/full.tpl
+MatchFile=full/video.tpl
+Subdir=templates
+Match[class_identifier]=video
+
+
+
+*/ ?>
