cc221a8accbb4ed9e49b5ac91b0f0dc89c5b3956
7x <info@se7enx.com>
2024-09-05 05:11:43 -0700

    Update ezinfo.php bump version number

--- ezinfo.php
+++ ezinfo.php
@@ -13,7 +13,7 @@ class sevenx_themes_simpleInfo
     public static function info()
     {
         return array( 'Name' => "<a href='https://github.com/se7enxweb/sevenx_themes_simple'>7x Themes : Simple</a>",
-                      'Version' => "1.0.1",
+                      'Version' => "1.0.2",
                       'Copyright' => "Copyright (C) 1999 - 2024 <a href='https://se7enx.com' title='7x'>Brookins Consulting</a>",
                       'License' => "GNU General Public License v2.0 (or later)",
                       'info_url' => "https://github.com/se7enxweb/sevenx_themes_simple"
