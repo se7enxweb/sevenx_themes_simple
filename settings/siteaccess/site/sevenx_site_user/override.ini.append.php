cc899d61e9c46724d3580ec46d1f0cf15794cb01
7x <info@se7enx.com>
2026-04-28 16:38:45 -0700

    Added: Added extension based siteaccess default settings for site siteaccess matching sevenx_site_user settings expectations. Bugfix for default installation usage of the siteaccess name site for default user siteaccess pattern/convention used by ecosystem. Bugfix.

---
+++ settings/siteaccess/site/sevenx_site_user/override.ini.append.php
@@ -0,0 +1,35 @@
+<?php /* #?ini charset="utf-8"?
+
+[embed_store]
+Source=node/view/embed.tpl
+MatchFile=full/folder.tpl
+Subdir=templates
+Match[class_identifier]=folder
+Match[node]=67
+
+[full_video]
+Source=node/view/full.tpl
+MatchFile=full/video.tpl
+Subdir=templates
+Match[class_identifier]=video
+
+[responsive_gallery_view_full]
+Source=node/view/full.tpl
+MatchFile=view/full/gallery.tpl
+Subdir=templates
+Match[class_identifier]=gallery
+
+[responsive_image_view_image_gallery]
+Source=node/view/image_gallery.tpl
+MatchFile=view/image_gallery/image.tpl
+Subdir=templates
+Match[class_identifier]=image
+
+[responsive_video_view_image_gallery]
+Source=node/view/image_gallery.tpl
+MatchFile=view/image_gallery/video.tpl
+Subdir=templates
+Match[class_identifier]=video
+
+
+*/ ?>
